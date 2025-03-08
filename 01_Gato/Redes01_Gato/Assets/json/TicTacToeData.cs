84752fc1f5f05404c10c91651fb12c86a89aa984
Yiztino <luiselp.080972@gmail.com>
2025-03-08 11:54:55 -0600

    Subiendo juego basico con carpetas listas
    Subo el juego, las carpetas necesarias y el repo en general (primer commit)

---
+++ 01_Gato/Redes01_Gato/Assets/json/TicTacToeData.cs
@@ -0,0 +1,19 @@
+[System.Serializable]
+public class TicTacToeData
+{
+    public int actual;
+    public int round;
+    public string score1;
+    public string score2;
+    public int[] board;
+    override public string ToString()
+    {
+        string data = "actual:" + actual + "\nround:" + round + "\nscore1" + score1 + "\nscore2" + score2 + "\nboard\n";
+        foreach (var item in board)
+        {
+            data += item + "\n";
+        }
+        return data;
+    }
+}
+
