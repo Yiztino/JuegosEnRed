7a18aae0adac760dc648a1e16997dea214d81fc6
Yiztino <124330425+Yiztino@users.noreply.github.com>
2025-03-21 18:53:44 -0600

    Subiendo practica 03

---
+++ 03_Nodejs/01_rectangulo.js
@@ -0,0 +1,35 @@
+class Rectangulo {
+  constructor(alto, ancho) {
+    this.alto = alto;
+    this.ancho = ancho;
+  }
+  // Getter
+  get area() {
+    return this.calcArea();
+  }
+  get perimetro() {
+    return this.calcPerimetro();
+  }
+  // Método
+  calcArea() {
+    return this.alto * this.ancho;
+  }
+
+  calcPerimetro(){
+    return (this.alto*2)+(this.ancho*2)
+  }
+
+  *obtenerLados(){
+    yield this.alto; 
+    yield this.ancho;
+    yield this.alto; 
+    yield this.ancho;
+  }
+}
+
+let cuadrado = new Rectangulo(10,10);
+
+
+
+console.log("Area: "+cuadrado.area);
+console.log("Perimetro: "+cuadrado.perimetro);
